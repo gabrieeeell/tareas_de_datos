80e827a071a0dca94af654f60a1dbb561beb30f6
Frisvy <gordenes@usm.cl>
2025-05-23 15:14:40 -0400

    la funcion leer mapa, no esta terminada, de momento devuelve un arreglo que almacena el dato de todas las habitaciones, con la informacion de estas

--- tarea_2/main.cpp
+++ tarea_2/main.cpp
@@ -3,16 +3,32 @@
 #include <unistd.h>
 #include <ctime>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 using namespace std;
 
-struct habitacion{
+struct Habitacion{
 	int numero;
 	string nombre;
 	string descripcion;
 	string tipo;
 };
+
+struct NodoMapa{
+	Habitacion habitacion;
+	NodoMapa *hijo1;
+	NodoMapa *hijo2;
+	NodoMapa *hijo3;
+};
+
+
+struct arco{
+	int desde; 
+	int hacia; 
+};
+
+
 
 struct enemigo{
 	string nombre;
@@ -20,15 +36,62 @@ struct enemigo{
 	int ataque;
 	int precision;
 	int probabilidad;
-}
-
+};
 
 class Jugador {
+private:
 	int vida;
 	int ataque;
 	float precision;
 	int recuperacion;
+public:
+	
 };
+
+void leer_mapa(string nombre_archivo){
+	int total_habitaciones;
+	string linea;
+	ifstream mapa(nombre_archivo);
+	if(!mapa.is_open()){
+		cout << "No se pudo abrir el archivo"<< endl;
+		return ;
+	}
+	while (getline(mapa, linea)){
+		if (linea == "HABITACIONES"){
+			getline(mapa, linea);
+			total_habitaciones = stoi(linea); //stoi transforma la linea a un int
+			Habitacion *habitaciones = new Habitacion[total_habitaciones];
+			
+			for (int i = 0;i < total_habitaciones; i++){
+				getline(mapa, linea);
+				
+				size_t espacio = linea.find(" ");
+				habitaciones[i].numero = stoi(linea.substr(0, espacio));
+				
+				size_t parentesis = linea.find ("(");
+				habitaciones[i].nombre = linea.substr(espacio + 1, parentesis - espacio - 2 );
+				
+				size_t parentesis_final = linea.find(")");
+				habitaciones[i].tipo = linea.substr(parentesis + 1, parentesis_final - parentesis - 1 );
+				
+				getline(mapa,linea);
+				habitaciones[i].descripcion = linea ;
+				}
+
+			break;
+		
+		}
+	
+	}
+	
+	
+	
+	
+	mapa.close();
+}
+
+
+
 
 void mostrar_generacion_texto(string texto) {
 	for (int i = 0; i < texto.length(); i++) {
@@ -46,5 +109,8 @@ int main() {
 	int random_number = rand();
 	string word = "Esta linea es para generar una seed aleatoria para los numeros random";
 	mostrar_generacion_texto(word);
+	leer_mapa("ejemplo.map");
+	
+	
 	return 0;
 }
